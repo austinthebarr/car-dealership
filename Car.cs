d866811ccff3757f869d92d89d836aed4c91b902
Austin Barr and Hisato Kawaminami <austin@>
2018-06-25 11:56:30 -0700

    added private class and public constructor

--- Car.cs
+++ Car.cs
@@ -3,19 +3,64 @@ using System.Collections.Generic;
 
 class Car
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-  public string Color;
-  public int Year;
+  private string _makeModel;
+  private int _price;
+  private int _miles;
+  private string _color;
+  private int _year;
 
-  public Car(string carMakeModel, int carPrice, int carMiles, string carColor = "black", int carYear = 2010)
+  public void SetMakeModel(string newMakeModel)
   {
-    MakeModel = carMakeModel;
-    Price = carPrice;
-    Miles = carMiles;
-    Color = carColor;
-    Year = carYear;
+    _makeModel = newMakeModel;
+  }
+  public string GetMakeModel()
+  {
+    return _makeModel;
+  }
+
+  public void SetPrice(int newPrice)
+  {
+    _price = newPrice;
+  }
+  public int GetPrice()
+  {
+    return _price;
+  }
+
+  public void SetMiles(int newMiles)
+  {
+    _miles = newMiles;
+  }
+  public int GetMiles()
+  {
+    return _miles;
+  }
+
+  public void SetColor(string newColor)
+  {
+    _color = newColor;
+  }
+  public string GetColor()
+  {
+    return _color;
+  }
+
+  public void SetYear(int newYear)
+  {
+    _year = newYear;
+  }
+  public int GetYear()
+  {
+    return _year;
+  }
+  
+  public Car(string carMakeModel, int carPrice, int carMiles, int carYear = 2010, string carColor = "black")
+  {
+    _makeModel = carMakeModel;
+    _price = carPrice;
+    _miles = carMiles;
+    _color = carColor;
+    _year = carYear;
   }
 }
 
@@ -23,18 +68,18 @@ public class Program
 {
   public static void Main()
   {
-    Car porsche = new Car("Porsche 911", 114991, 7864 );
+    Car porsche = new Car("Porsche", 10000, 1000, 2016, "yellow");
     Car ford = new Car("Ford Raptor", 60000, 2500);
     Car bmw = new Car("BMW M5", 110000, 25000);
-    Car mercedes = new Car("Mercedes AMG", 120000, 32000);
-    Car gio = new Car("Gio Storm", 2000, 89, "white", 1999);
+    Car mercedes = new Car("Mercedes AMG", 120000, 3200);
+    Car gio = new Car("Gio Storm", 2000, 89, 1999);
 
     List<Car> Cars = new List<Car>() { porsche, ford, bmw, mercedes, gio };
 
     foreach (Car automobile in Cars)
     {
-      Console.WriteLine(automobile.MakeModel);
-      Console.WriteLine(automobile.Color);
+      Console.WriteLine(automobile.GetMakeModel());
+      Console.WriteLine(automobile.GetColor());
     }
     Console.WriteLine("Enter max price: ");
     string stringMaxPrice = Console.ReadLine();
@@ -44,15 +89,15 @@ public class Program
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.Price <= maxPrice)
+      if (automobile.GetPrice() <= maxPrice)
       {
         CarsMatchingSearch.Add(automobile);
       }
     }
     foreach (Car automobile in CarsMatchingSearch)
     {
-      Console.WriteLine(automobile.MakeModel);
-      Console.WriteLine(automobile.Color);
+      Console.WriteLine(automobile.GetMakeModel());
+      Console.WriteLine(automobile.GetColor());
     }
   }
 
